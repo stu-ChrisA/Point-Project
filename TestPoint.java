9774a4bb6bd292a85b5950bf0c7de38a1b35d36e
stu-ChrisA <92584525+stu-ChrisA@users.noreply.github.com>
2022-01-19 13:05:55 -0500

    Create TestPoint.java

---
+++ TestPoint.java
@@ -0,0 +1,8 @@
+public class TestPoint 
+{
+    public static void main(String[] args) 
+    {
+        Point x = new Point();
+        System.out.println(x.manhattanDistance(-5,7,3,-10)); 
+    }
+}
