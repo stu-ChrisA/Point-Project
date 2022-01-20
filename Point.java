27d8f946fec765ad766aab257533264ca976d36f
stu-ChrisA <cacosta@stu.socsd.org>
2022-01-19 21:29:10 -0500

    Added Final Touches
    It works as intended

--- Point.java
+++ Point.java
@@ -34,7 +34,13 @@ public class Point
     }
     public boolean isCollinear(Point p1, Point p2)
     {
-        
+        if ((this.x == p1.x && this.x == p2.x) || (this.y == p1.y && this.y == p2.y)) {
+            return true;
+        } else {
+            double slope1 = (double)(this.y - p1.y) / (double)(this.x - p1.x);
+            double slope2 = (double)(this.y - p2.y) / (double)(this.x - p2.x);
+            return slope1 == slope2; 
+        }
     }
 
 }
