2c1ddc033cde1dc2c7df748228e06a0aafad0b0f
stu-ChrisA <92584525+stu-ChrisA@users.noreply.github.com>
2022-01-20 21:41:34 -0500

    Added some notes

--- TestPoint.java
+++ TestPoint.java
@@ -9,8 +9,12 @@ public class TestPoint
         Point p4 = new Point(3,-10);
         Point p5 = new Point(3,10);
         Point p6 = new Point(6,8);
+        
+        //Testing manhattanDistance: 
         System.out.println(p2.manhattanDistance(p3)); 
         System.out.println(p3.manhattanDistance(p4)); 
+        
+        //Testing isCollinear:
         System.out.println(p2.isCollinear(p4,p5));
         System.out.println(p2.isCollinear(p3,p4));
         System.out.println(p1.isCollinear(p2,p6));
